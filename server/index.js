86c2cb71648aa81a5153f38f225fc59215912bba
Arsalan-Habib <syedarsalanhabib@gmail.com>
2024-01-29 01:30:41 +0500

    refactor: refactor to use mongodb instead of memory

--- server/index.js
+++ server/index.js
@@ -1,67 +1,53 @@
 const express = require("express");
-// const sqlite3 = require("sqlite3").verbose();
-// const db = new sqlite3.Database("database.db");
+const { connectDB } = require("./db");
+const { getLeaderboard, addBenchmark, getRank } = require("./benchmark");
 
 const DISCORD_APPLICATION_ID = process.env.DISCORD_APPLICATION_ID;
 const DISCORD_PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 
-// using in memory db for simplicity.
-const db = {};
+connectDB();
 
 const app = express();
 const port = 80;
 
-const getAvgScore = (user) => {
-    const { singleCoreScore, multiCoreScore } = db[user];
-    return (singleCoreScore + multiCoreScore) / 2;
-};
-
-const getLeaderboard = () => {
-    // sorting users by avg of single and multi core test cores.
-    const sortedUsers = Object.keys(db).sort(
-        (a, b) => getAvgScore(a) - getAvgScore(b)
-    );
-
-    const leaderboard = sortedUsers.map((user) => ({
-        username: user,
-        score: db[user],
-    }));
-
-    return leaderboard;
-};
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Endpoint to get leaderboard
-app.get("/", (req, res) => {
-    const leaderboard = getLeaderboard();
+app.get("/", async (req, res) => {
+    const leaderboard = await getLeaderboard();
 
     return res.status(200).json({
         message: "Leaderboard fetched successfully!",
         data: leaderboard,
-        variables: {
-            DISCORD_APPLICATION_ID,
-            DISCORD_PUBLIC_KEY,
-            DISCORD_BOT_TOKEN,
-        },
     });
 });
 
 // Endpoint to submit score
-app.post("/submit-score", (req, res) => {
+app.post("/score", async (req, res) => {
     const { username, score } = req.body;
+
+    console.log(req.body.score);
+
     if (!username || !score) {
-        return res.status(400).send("Missing username or score");
+        return res.status(400).send("Missing username or score details.");
     }
 
-    db[`${username}`] = score;
+    const benchmark = {
+        username,
+        ...score,
+    };
 
-    const leaderboard = getLeaderboard();
+    await addBenchmark(benchmark);
 
-    res.status(200).json({
+    const rank = await getRank(score.overallScore);
+
+    return res.status(200).json({
         message: "Score submitted successfully!",
-        data: leaderboard,
+        data: {
+            rank,
+        },
     });
 });
 
